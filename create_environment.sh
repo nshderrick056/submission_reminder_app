f1d56a4f0b04113a43d57e016c1b89183efd8075
nshderrick056 <d.gatete@alustudent.com>
2025-06-16 08:38:50 -0700

    creating the config.sh and reminder.sh

--- create_environment.sh
+++ create_environment.sh
@@ -9,3 +9,35 @@ sub_dir="submission_reminder_${name}"
 
 mkdir -p $sub_dir/{app,modules,assests,config}
 
+# creating the config.env
+
+cat << EOF > $sub_dir/config/config.env
+
+# This is the config file
+ASSIGNMENT="Shell Navigation"
+DAYS_REMAINING=2
+
+EOF
+
+# creating the reminder.sh
+
+cat << EOF > $sub_dir/app/reminder.sh
+
+#!/bin/bash
+
+# Source environment variables and helper functions
+source ./config/config.env
+source ./modules/functions.sh
+
+# Path to the submissions file
+submissions_file="./assets/submissions.txt"
+
+# Print remaining time and run the reminder function
+echo "Assignment: $ASSIGNMENT"
+echo "Days remaining to submit: $DAYS_REMAINING days"
+echo "--------------------------------------------"
+
+check_submissions $submissions_file
+
+EOF
+
